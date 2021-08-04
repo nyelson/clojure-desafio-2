35e94ce32ab33609066162efa09808f8f07becd0
nyelson.barbosa <nyelson.barbosa@nubank.com.br>
2021-08-04 14:04:07 -0300

    Alterando método de adicionar compra na lista de compras

---
+++ test/clojure_desafio_2/logic_test.clj
@@ -0,0 +1,8 @@
+(ns clojure-desafio-2.logic-test
+  (:require [clojure.test :refer :all]
+            [clojure-desafio-2.logic :refer :all]))
+
+;(deftest adicionar-compra-test
+;  (testing "Testando o método de adicionar compra na lista de compras"
+;    (let [lista])
+;    (is (= ()))))
