860310157981415758e3b1be6f604094baa6dfe0
nyelson.barbosa <nyelson.barbosa@nubank.com.br>
2021-08-04 17:39:59 -0300

    Construindo método para listar compras por cliente

--- src/clojure_desafio_2/logic.clj
+++ src/clojure_desafio_2/logic.clj
@@ -7,7 +7,8 @@
 (println "\n")
 
 (def lista-de-compras w.db/compras)
-
+(def lista-de-cartoes w.db/cartao)
+(def lista-de-clientes w.db/cliente)
 
 (defn agrupa-categorias
   "Agrupa e retorna uma lista de acordo com a categoria"
@@ -23,8 +24,6 @@
   "Retorna o somatório total dos valores passados pela lista no parâmetro"
   [lista]
   (->> lista (map :valor) (reduce +)))
-
-
 
 (defn compras-por-estabelecimento-ou-valor
   [coluna filtro compra]
@@ -48,17 +47,59 @@
   [compra lista-de-compras]
   (conj lista-de-compras compra))
 
+;(println "\nAssociando os objetos:" (assoc-in w.db/cliente [:cartaoRef] (assoc-in w.db/cartao [:comprasRef] lista-de-compras)))
+
+;(def lista-de-cartoes (assoc-in lista-de-cartoes [:comprasRef] lista-de-compras))
+;(conj lista-de-cartoes lista-de-compras)
+;(def lista-de-cartoes)
+;(println (assoc-in lista-de-cartoes [:comprasRef] lista-de-compras))
 
 (println "\n\n\natribuição a uma nova lista de uma nova compra")
+
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 10 20), :valor 1000, :estabelecimento "Adidas",       :categoria "Vestuário"   } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 10 21), :valor 250,  :estabelecimento "Adidas",       :categoria "Vestuário"   } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 03 13), :valor 40,   :estabelecimento "Burguer King", :categoria "Restaurante" } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 05 29), :valor 300,  :estabelecimento "Nike",         :categoria "Vestuário"   } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 30), :valor 50,   :estabelecimento "Cinemark",     :categoria "Cinema"      } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 27), :valor 5,    :estabelecimento "Padaria",      :categoria "Alimentos"   } lista-de-compras))
+(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 25), :valor 70,   :estabelecimento "Boliche",      :categoria "Aleatório"   } lista-de-compras))
+
+(def lista-de-cartoes (assoc-in lista-de-cartoes [:comprasRef] lista-de-compras))
+(def lista-de-clientes (assoc-in lista-de-clientes [:cartaoRef] lista-de-cartoes))
+
+(println lista-de-cartoes)
+(println lista-de-clientes)
+
+;{:nome Nyelson, :cpf 22667862023, :email nyelson.barbosa@nubank.com.br,
+;   :cartaoRef {:numero 5410133996442556, :cvv 984, :validade #object[java.time.LocalDateTime 0x133f86bc 2022-10-20T00:00], :limite 10000,
+;     :comprasRef [{:data #object[java.time.LocalDateTime 0x21a66db1 2021-10-20T00:00], :valor 1000, :estabelecimento Adidas, :categoria Vestuário}
+;                  {:data #object[java.time.LocalDateTime 0x4526d37d 2021-10-21T00:00], :valor 250, :estabelecimento Adidas, :categoria Vestuário}
+;                  {:data #object[java.time.LocalDateTime 0x3e01d372 2021-03-13T00:00], :valor 40, :estabelecimento Burguer King, :categoria Restaurante}
+;                  {:data #object[java.time.LocalDateTime 0x10cdaee8 2021-05-29T00:00], :valor 300, :estabelecimento Nike, :categoria Vestuário}
+;                  {:data #object[java.time.LocalDateTime 0x29482a5 2021-12-30T00:00], :valor 50, :estabelecimento Cinemark, :categoria Cinema}
+; {:data #object[java.time.LocalDateTime 0x715a14b1 2021-12-27T00:00], :valor 5, :estabelecimento Padaria, :categoria Alimentos} {:data #object[java.time.LocalDateTime 0x41f0d3ab 2021-12-25T00:00], :valor 70, :estabelecimento Boliche, :categoria Aleatório}]}}
+
+;(defn lista-compras-por-cliente
+;  [cpf-cliente lista-de-clientes]
+;  (->> lista-de-clientes
+;            (:cpf cpf-cliente)))
 ;
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 10 20), :valor 1000, :estabelecimento "Adidas",       :categoria "Vestuário"   } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 10 21), :valor 250,  :estabelecimento "Adidas",       :categoria "Vestuário"   } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 03 13), :valor 40,   :estabelecimento "Burguer King", :categoria "Restaurante" } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 05 29), :valor 300,  :estabelecimento "Nike",         :categoria "Vestuário"   } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 30), :valor 50,   :estabelecimento "Cinemark",     :categoria "Cinema"      } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 27), :valor 5,    :estabelecimento "Padaria",      :categoria "Alimentos"   } lista-de-compras))
-;(def lista-de-compras (adicionar-compra {:data (local-date-time 2021 12 25), :valor 70,   :estabelecimento "Boliche",      :categoria "Aleatório"   } lista-de-compras))
+;(println "Usuario:" (lista-compras-por-cliente "0" lista-de-clientes))
+
+;#(= "22667862023" (:cpf %)
+
+;(defn compara-cpf
+;  [cpf cliente]
+;  (println cliente)
+;  (= cpf (:cpf cliente)))
 ;
+;;(println (filter #(compara-cpf "22667862023" %) lista-de-clientes))
+;(println (->> lista-de-clientes
+;              (filter #(compara-cpf 22667862023 %))))
+
+
+
+
 
 ;acho estranho a funcao já dizer que vai usar esta lista de compras na definição dela, se eu quiser utilizar outra
 ; não consigo
