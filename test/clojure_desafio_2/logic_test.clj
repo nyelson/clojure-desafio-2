c6c267396d20534ebff4f84f40b20ab3c4bcd00b
nyelson.barbosa <nyelson.barbosa@nubank.com.br>
2021-08-04 14:45:55 -0300

    Construindo o teste de adicionar compra

--- test/clojure_desafio_2/logic_test.clj
+++ test/clojure_desafio_2/logic_test.clj
@@ -2,7 +2,10 @@
   (:require [clojure.test :refer :all]
             [clojure-desafio-2.logic :refer :all]))
 
-;(deftest adicionar-compra-test
-;  (testing "Testando o método de adicionar compra na lista de compras"
-;    (let [lista])
-;    (is (= ()))))
+(use 'java-time)
+
+(deftest adicionar-compra-test
+  (testing "Testando o método de adicionar compra na lista de compras"
+    [lista-de-compras]
+    (is (= 0 (count lista-de-compras)))
+    (is (= 1 (count (adicionar-compra {:data (local-date-time 2021 10 20), :valor 1000, :estabelecimento "Adidas", :categoria "Vestuário"} lista-de-compras))))))
